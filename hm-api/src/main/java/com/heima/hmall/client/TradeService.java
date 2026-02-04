8293ff561801b5c78f30ca1c3cf7e8de4ec14bcc
ljj <3267275493@qq.com>
2026-02-04 18:45:38 +0800

    设置远程调用客户端降级逻辑wc

--- hm-api/src/main/java/com/heima/hmall/client/TradeService.java
+++ hm-api/src/main/java/com/heima/hmall/client/TradeService.java
@@ -1,11 +1,12 @@
 package com.heima.hmall.client;
 
 import com.heima.hmall.config.DefaultFeignConfig;
+import com.heima.hmall.fallback.TradeClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(value = "trade-service",fallbackFactory = TradeService.class,configuration = DefaultFeignConfig.class)
+@FeignClient(value = "trade-service",fallbackFactory = TradeClientFallback.class,configuration = DefaultFeignConfig.class)
 public interface TradeService {
     @PutMapping("/orders/{orderId}")
     public void markOrderPaySuccess(@PathVariable("orderId") Long orderId) ;
--- hm-api/src/main/java/com/heima/hmall/client/UserService.java
+++ hm-api/src/main/java/com/heima/hmall/client/UserService.java
@@ -1,11 +1,12 @@
 package com.heima.hmall.client;
 
 import com.heima.hmall.config.DefaultFeignConfig;
+import com.heima.hmall.fallback.UserClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "user-service",fallbackFactory = UserService.class,configuration = DefaultFeignConfig.class)
+@FeignClient(value = "user-service",fallbackFactory = UserClientFallback.class,configuration = DefaultFeignConfig.class)
 public interface UserService {
     @PutMapping("/users/money/deduct")
     public void deductMoney(@RequestParam("pw") String pw, @RequestParam("amount") Integer amount);
